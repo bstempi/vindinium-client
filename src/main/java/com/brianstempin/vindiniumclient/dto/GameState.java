d11e5bf9a4d3caf03c319a02982b7b72f62290b3
Brian Stempin <brian.stempin@gmail.com>
2014-09-22 21:56:52 -0400

    Whoops -- I somehow forgot to generate getter/setters for Position.y.

--- src/main/java/com/brianstempin/vindiniumclient/dto/GameState.java
+++ src/main/java/com/brianstempin/vindiniumclient/dto/GameState.java
@@ -262,12 +262,20 @@ public class GameState {
 
         }
 
-        public Position(int x) {
+        public int getX() {
+            return x;
+        }
+
+        public int getY() {
+            return y;
+        }
+
+        public void setX(int x) {
             this.x = x;
         }
 
-        public int getX() {
-            return x;
+        public void setY(int y) {
+            this.y = y;
         }
     }
 }
